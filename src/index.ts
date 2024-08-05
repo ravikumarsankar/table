b235af9fe7d3f3a01b443ed05f774f51c924fc14
Ravikumar S <sravi20071999@gmail.com>
2024-08-05 17:26:07 +0530

    fix(console): fixed console error

--- src/index.ts
+++ src/index.ts
@@ -465,9 +465,9 @@ export default class Table {
     const v = _cells.get(row, col);
 
     // If in edit mode and the cell has a formula, return the formula instead of the calculated value
-    if (this._editor && this._formulas[row][col]) {
-      // return this._formulas[row][col];
-    }
+    // if (this._editor && this._formulas[row][col]) {
+    //   // return this._formulas[row][col];
+    // }
 
     return v != null ? v[2] : v;
   }
